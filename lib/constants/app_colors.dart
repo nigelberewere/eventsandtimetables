d9fff18561efa0f6ca5c61a988e6a781c3c92bed
nigelberewere <nigelberewere@gmail.com>
2026-05-05 13:10:17 +0200

    update app colours

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Theme colors
+  static const Color amberSmoke = Color(0xFFF2E0D0);
+  static const Color blueMirage = Color(0xFF5E88B0);
+  static const Color blueMirageFallback = Color(0xFF5C6D7C);
+
+  // Additional utility colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color darkGray = Color(0xFF333333);
+  static const Color lightGray = Color(0xFFF5F5F5);
+  static const Color dividerGray = Color(0xFFE0E0E0);
+
+  // Status colors
+  static const Color success = Color(0xFF4CAF50);
+  static const Color error = Color(0xFFE53935);
+  static const Color warning = Color(0xFFFFA726);
+  static const Color info = Color(0xFF29B6F6);
+}
