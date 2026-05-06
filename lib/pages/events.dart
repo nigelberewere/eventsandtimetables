42d9a29f17365b321d638341cff26228e4827864
pineapppppleee <munyangadzitrish@gmail.com>
2026-05-06 14:27:32 +0200

    fix: removed add events functions for general users

--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:provider/provider.dart';
 import 'theme_provider.dart';
-import 'addEvents.dart';
+import 'addEvent.dart';
 import 'home.dart';
 import '../widgets/bottom_dock.dart';
 
